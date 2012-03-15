35070f5e4e519ff3f1aa41f75ac530c30055ea3c
Krzysztof Urbaniak <urban@fail.pl>
2012-03-15 09:59:03 +0100

    added creating directories when installing dotfiles

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@
 
 from fabric.api import prompt
 from os.path import abspath, dirname, exists, expanduser, join
-from os import symlink, walk, remove
+from os import symlink, walk, remove, makedirs
 from shutil import rmtree
 
 
@@ -19,7 +19,14 @@ def install():
 			fname = join(top, fn)
 
 			src = abspath(fname)
-			dst = expanduser('~') + fname.replace('.symlink', '').replace(DOTFILES_DIR, '')
+			path = fname.replace('.symlink', '').replace(DOTFILES_DIR, '').lstrip('/')
+			dst = join(expanduser('~'), path)
+
+			if '/' in path:
+				try:
+					makedirs('/'.join(dst.split('/')[:-1]))
+				except OSError:
+					pass
 
 			if exists(dst):
 				r = prompt('File %s already exists. Overwrite?' % dst, default='n')
